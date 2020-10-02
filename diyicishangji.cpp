fa6028c6d662c987dbf56fd70c6e9f06a72cb93b
heiguangzhuangyiming <72078246+heiguangzhuangyiming@users.noreply.github.com>
2020-10-02 08:56:57 +0800

    Update diyicishangji.cpp

--- diyicishangji.cpp
+++ diyicishangji.cpp
@@ -5,129 +5,135 @@
 typedef struct student SS;
 struct student
 {
-	int ID;
-	char name[10];
-	float usualscore;
-	float labscore;
-	float finalscore;
-	float totalscore;
-	int order;
-}stu[];
+    int ID;
+    char name[10];
+    float usualscore;
+    float labscore;
+    float finalscore;
+    float totalscore;
+    int order;
+} stu[];
 void putdata(SS stu[], int N)
 {
-	int i;
-	printf("Çë°´ÕÕÈçÏÂ¸ñÊ½ÊäÈëÑ§ÉúĞÅÏ¢£ºÑ§ºÅ£¬ĞÕÃû£¬Æ½Ê±³É¼¨£¬ÆÚÄ©³É¼¨\n");
-	for (i = 0; i < N; i++)
-	{
-		printf("µÚ%d¸öÑ§Éú£º", i + 1);
-		scanf("%d,%s%,%f,%f,%f", &stu[i].ID, &stu[i].name, &stu[i].usualscore,
-			&stu[i].labscore, &stu[i].finalscore);
-		printf("\n");
-	}
-	printf("³É¼¨Â¼ÈëÍê±Ï");
+    int i;
+    printf("ï¿½ë°´ï¿½ï¿½ï¿½ï¿½ï¿½Â¸ï¿½Ê½ï¿½ï¿½ï¿½ï¿½Ñ§ï¿½ï¿½ï¿½ï¿½Ï¢ï¿½ï¿½Ñ§ï¿½Å£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ½Ê±ï¿½É¼ï¿½ï¿½
+           ï¿½ï¿½ï¿½Ä©ï¿½É¼ï¿½\n");
+    for (i = 0; i < N; i++)
+    {
+        printf("ï¿½ï¿½%dï¿½ï¿½Ñ§ï¿½ï¿½ï¿½ï¿½", i + 1);
+        scanf("%d,%s%,%f,%f,%f", &stu[i].ID, &stu[i].name, 
+              &stu[i].usualscore,
+              &stu[i].labscore, &stu[i].finalscore);
+        printf("\n");
+    }
+    printf("ï¿½É¼ï¿½Â¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
 }
 
 SS* orderdata(SS stu[], int N)
 {
-	FILE *fp = NULL;
-	int count = 0;
-	int index = 0;
-	fp = fopen("data.text", "r");
-	if (fp != NULL)
-	{
-		fscanf(fp, "%d", &count);
-		*N = count;
-	}
-	else
-	{
-		printf("failed to open the info file\n");
-		getchar();
-	}
-	printf("Ñ§ÉúÊıÄ¿Îª£º%d\n", count);
-	stu = (SS*)malloc(count * sizeof(SS));
-	while (!(feof(fp)))
-	{
-		fscanf(fp, "%d", &stu[i].ID);
-		fscanf(fp, "%f", &stu[i].name);
-		fscanf(fp, "%d", &stu[i].usualscore);
-		fscanf(fp, "%d", &stu[i].labscore);
-		fscanf(fp, "%d", &stu[i].finalscore);
-		i++;
-		count--;
-	}
+    FILE *fp = NULL;
+    int count = 0;
+    int index = 0;
+    fp = fopen("data.text", "r");
+    if (fp != NULL)
+    {
+        fscanf(fp, "%d", &count);
+        *N = count;
+    }
+    else
+    {
+        printf("failed to open the info file\n");
+        getchar();
+    }
+    printf("Ñ§ï¿½ï¿½ï¿½ï¿½Ä¿Îªï¿½ï¿½%d\n", count);
+    stu = (SS*)malloc(count * sizeof(SS));
+    while (!(feof(fp)))
+    {
+        fscanf(fp, "%d", &stu[i].ID);
+        fscanf(fp, "%f", &stu[i].name);
+        fscanf(fp, "%d", &stu[i].usualscore);
+        fscanf(fp, "%d", &stu[i].labscore);
+        fscanf(fp, "%d", &stu[i].finalscore);
+        i++;
+        count--;
+    }
 
-	fprintf(fp, student[N]);
-	printf("Êı¾İÒÑ¾­ÊäÈë");
-	if (fp)
-	{
-		fclose(fp);
-		fp = NULL;
-	}
+    fprintf(fp, student[N]);
+    printf("ï¿½ï¿½ï¿½ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½ï¿½ï¿½");
+    if (fp)
+    {
+        fclose(fp);
+        fp = NULL;
+    }
 
 }
 int i, j;
 SS c;
 for (i = 0; i < N; i++)
 {
-	stu[i].totalscore = 0.2*stu[i].usualscore + 0.2*stu[i].labscore + 0.6*stu[i].finalscore;
+    stu[i].totalscore = 0.2*stu[i].usualscore + 0.2*stu[i].
+                        labscore + 0.6*stu[i].finalscore;
 }
 for (i = 0; i < n; i++)
 {
-	qsort(&(stu[0]), N, sizeof(stu[0]), cmpBigtoSmall)
-		for (i = 0; i < N; i++)
-			printf("%d,%s,%f", stu[i].ID, stu[i].name, stu[i].totalscore);
-}*/
+    qsort(&(stu[0]), N, sizeof(stu[0]), cmpBigtoSmall)
+    for (i = 0; i < N; i++)
+        printf("%d,%s,%f", stu[i].ID, stu[i].name, stu[i].
+               totalscore);
+}
+*/
 void findscore(SS stu[], int N)
 {
-	scanf("%d", &stu.ID);
-	int i, k;
-	for (i = 0; i < N; i++)
-	{
-		if (stu[i].ID = stu.ID)
-			k = i;
-	}
-	FILE *fp = NULL;
-	fp = fopen("data.text", "w");
-	fprintf("%f,%f,%f,%f", stu[k].usualscore, stu[k].labscore, stu[k].finalscore, stu[k].totalscore);
+    scanf("%d", &stu.ID);
+    int i, k;
+    for (i = 0; i < N; i++)
+    {
+        if (stu[i].ID = stu.ID)
+            k = i;
+    }
+    FILE *fp = NULL;
+    fp = fopen("data.text", "w");
+    fprintf("%f,%f,%f,%f", stu[k].usualscore, stu[k].
+            labscore, stu[k].finalscore, stu[k].totalscore);
 
 }
 float analysedata(SS stu[], int N)
 {
-	orderdata(stu[], N);
-	FILE *fp = NULL;
-	fp = fopen("data.text", "w");
-	int i;
-	float averagescore;
-	float b[100];
-	for (i = 0; i < n; i++)
-	{
-		fprintf("%f", &stu[i].totalscore);
-		float sum1;
-		sum1 = 0;
-		sum1 += stu[i].totalscore;
+    orderdata(stu[], N);
+    FILE *fp = NULL;
+    fp = fopen("data.text", "w");
+    int i;
+    float averagescore;
+    float b[100];
+    for (i = 0; i < n; i++)
+    {
+        fprintf("%f", &stu[i].totalscore);
+        float sum1;
+        sum1 = 0;
+        sum1 += stu[i].totalscore;
 
-	}
-	averagescore = sum1 / N;
-	for (i = 0; i < n; i++)
-	{
-		float sum2, variance;
-		sum2 = 0;
-		sum2 += pow(stu[i]totalscore - averagescore, 2);
-	}
-	variance = sum2 / n;
-	return variance;
+    }
+    averagescore = sum1 / N;
+    for (i = 0; i < n; i++)
+    {
+        float sum2, variance;
+        sum2 = 0;
+        sum2 += pow(stu[i]totalscore - averagescore, 2);
+    }
+    variance = sum2 / n;
+    return variance;
 }
 main()
 {
-	struct student;
-	SS stu[100];
-	int N;
-	float variance;
-	scanf("%d", &N);
-	putdata(stu, N);
-	orderdata(stu, N);
-	findscore(stu, N);
-	variance = analysedata(stu, N);
-	printf("%f", variance);
+    struct student;
+    SS stu[100];
+    int N;
+    float variance;
+    scanf("%d", &N);
+    putdata(stu, N);
+    orderdata(stu, N);
+    findscore(stu, N);
+    variance = analysedata(stu, N);
+    printf("%f", variance);
 
-}
+} 
